7f3332438d915c87949a76f5a7828564bdc9d58a
Osvaldo Trevino <svldtrvn58@gmail.com>
2023-04-26 04:48:36 -0500

    Fixed The Calculator

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const clear = document.querySelector(".clear");
 const letters = document.querySelectorAll(".l,.o,.e,.n");
 const img = document.querySelector("img");
 let leonSaying = document.querySelector(".cameo p");
-document.querySelector("h2").innerText = ":)";
 
+// LEON SECRET //
 letters.forEach((letter) => {
   letter.addEventListener("click", () => {
     if (
@@ -22,28 +22,68 @@ letters.forEach((letter) => {
     }
   });
 });
+/////////////////////////////////////////////
+
+document.querySelector("h2").innerText = ":)";
 
 function Calculator() {
   let values = "";
+  let holder = "";
   let maxAmountOfValues = 10;
   this.count = 0;
 
   this.shape = "rectangle";
   this.color = "black";
 
-  this.numberToAdd = function (value) {
+  this.addADigit = function (value) {
     values += value;
     this.count += 1;
+    showOnScreen();
   };
+
+  function showOnScreen() {
+    domScreen.innerText = values;
+  }
+
+  function showSmiliesResult(result) {
+    document.querySelector("h2").innerText = result;
+    setTimeout(() => {
+      document.querySelector("h2").innerText = ":)";
+    }, 2000);
+  }
 
   this.reset = function () {
     values = "";
     this.count = 0;
+    domScreen.innerText = "";
   };
 
   this.calculate = function () {
-    return eval(values);
+    let result;
+    try {
+      result = eval(values);
+      domScreen.innerText = result;
+      showSmiliesResult(result);
+      setTimeout(() => {
+        this.reset();
+      }, 2000);
+    } catch (error) {
+      alert("You can't do that: " + error);
+      this.reset();
+    }
   };
 }
 
-const calculator = new Calculator();
+const calc = new Calculator();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (button.innerText !== "=") {
+      calc.addADigit(button.innerText);
+    } else {
+      calc.calculate();
+    }
+  });
+});
+
+clear.addEventListener("click", () => calc.reset());
